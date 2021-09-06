03172f8031b69ca3022342eae620b98405cbc3cf
Igor Dias <igordias@Igors-MacBook-Pro.local>
2021-09-06 13:18:34 -0300

    added just a simple api

--- src/main/java/com/github/igordiasth/demoSpring/DemoSpringApplication.java
+++ src/main/java/com/github/igordiasth/demoSpring/DemoSpringApplication.java
@@ -2,12 +2,21 @@ package com.github.igordiasth.demoSpring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class DemoSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringApplication.class, args);
 	}
 
+	@GetMapping
+	public List<String> hello(){
+		return List.of("Hello", "World");
+	}
 }

ae38b47bc842476c9e199b26a29cdd66c8631f71
Igor Dias <igordias@Igors-MacBook-Pro.local>
2021-09-06 18:42:22 -0300

    added api layer, studentController

--- src/main/java/com/github/igordiasth/demoSpring/DemoSpringApplication.java
+++ src/main/java/com/github/igordiasth/demoSpring/DemoSpringApplication.java
@@ -1,33 +1,12 @@
 package com.github.igordiasth.demoSpring;
 
-import com.github.igordiasth.demoSpring.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class DemoSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringApplication.class, args);
-	}
-
-	@GetMapping
-	public List<Student> hello(){
-		return List.of(
-				new Student(
-						1L,
-						"Igor",
-						"igordiasth@gmail.com",
-						LocalDate.of(2000, Month.AUGUST, 21),
-						20
-				)
-		);
 	}
 }

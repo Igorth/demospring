37836badad5d714edcf40f3d334958f2cc73775c
Igor Dias <igordias@Igors-MacBook-Pro.local>
2021-09-06 13:33:47 -0300

    added student class

--- src/main/java/com/github/igordiasth/demoSpring/DemoSpringApplication.java
+++ src/main/java/com/github/igordiasth/demoSpring/DemoSpringApplication.java
@@ -1,10 +1,13 @@
 package com.github.igordiasth.demoSpring;
 
+import com.github.igordiasth.demoSpring.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,7 +19,15 @@ public class DemoSpringApplication {
 	}
 
 	@GetMapping
-	public List<String> hello(){
-		return List.of("Hello", "World");
+	public List<Student> hello(){
+		return List.of(
+				new Student(
+						1L,
+						"Igor",
+						"igordiasth@gmail.com",
+						LocalDate.of(2000, Month.AUGUST, 21),
+						20
+				)
+		);
 	}
 }

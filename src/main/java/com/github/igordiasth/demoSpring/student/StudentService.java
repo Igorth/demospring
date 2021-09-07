06e5aa7421ee97f2ecc3cfb7786de77df0c5bd9e
Igor Dias <igordias@Igors-MacBook-Pro.local>
2021-09-07 20:17:01 -0300

    added business layer

---
+++ src/main/java/com/github/igordiasth/demoSpring/student/StudentService.java
@@ -0,0 +1,19 @@
+package com.github.igordiasth.demoSpring.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+public class StudentService {
+    public List<Student> getStudents(){
+        return List.of(
+                new Student(
+                        1L,
+                        "Igor",
+                        "igordiasth@gmail.com",
+                        LocalDate.of(2000, Month.AUGUST, 21),
+                        20
+                )
+        );
+    }
+}

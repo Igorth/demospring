b83cf68dcadef628961965fc48a22425139326a8
Igor Dias <igordias@Igors-MacBook-Pro.local>
2021-09-11 19:05:52 -0300

    added jpa repository

--- src/main/java/com/github/igordiasth/demoSpring/student/StudentService.java
+++ src/main/java/com/github/igordiasth/demoSpring/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.github.igordiasth.demoSpring.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -8,15 +9,15 @@ import java.util.List;
 
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents(){
-        return List.of(
-                new Student(
-                        1L,
-                        "Igor",
-                        "igordiasth@gmail.com",
-                        LocalDate.of(2000, Month.AUGUST, 21),
-                        20
-                )
-        );
+        return studentRepository.findAll();
     }
 }

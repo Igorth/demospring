fe340c93223a7b292cbfa6aa4c316ade3fcae92a
Igor Dias <igordias@Igors-MacBook-Pro.local>
2021-09-11 19:31:01 -0300

    added saving students

---
+++ src/main/java/com/github/igordiasth/demoSpring/student/StudentConfig.java
@@ -0,0 +1,36 @@
+package com.github.igordiasth.demoSpring.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student igor = new Student(
+                    "Igor",
+                    "igordiasth@gmail.com",
+                    LocalDate.of(2000, Month.AUGUST, 21),
+                    20
+            );
+
+            Student laisa = new Student(
+                    "Laisa",
+                    "laisa@gmail.com",
+                    LocalDate.of(1988, Month.OCTOBER, 5),
+                    20
+            );
+
+            repository.saveAll(
+                    List.of(igor, laisa)
+            );
+        };
+    }
+}

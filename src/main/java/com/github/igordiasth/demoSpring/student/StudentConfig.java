08486e576fab2918fbebe6c681ef0fda9dee5e29
Igor Dias <igordias@Igors-MacBook-Pro.local>
2021-09-12 12:57:02 -0300

    added @transient age

--- src/main/java/com/github/igordiasth/demoSpring/student/StudentConfig.java
+++ src/main/java/com/github/igordiasth/demoSpring/student/StudentConfig.java
@@ -17,15 +17,13 @@ public class StudentConfig {
             Student igor = new Student(
                     "Igor",
                     "igordiasth@gmail.com",
-                    LocalDate.of(2000, Month.AUGUST, 21),
-                    20
+                    LocalDate.of(2000, Month.AUGUST, 21)
             );
 
             Student laisa = new Student(
                     "Laisa",
                     "laisa@gmail.com",
-                    LocalDate.of(1988, Month.OCTOBER, 5),
-                    20
+                    LocalDate.of(1988, Month.OCTOBER, 5)
             );
 
             repository.saveAll(
